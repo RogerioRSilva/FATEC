75392aeaec67abf4a0877bcce68b0ff289b36f3d
Rogério Rodrigues Silva <114150848+RogerioRSilva@users.noreply.github.com>
2023-03-14 20:19:52 -0300

    Exercicio Aula02 DWIII

--- 3º - Semestre/Desenvolvimento WEB II/Desenvolvimento React/primeiro-site/src/Components/Cadastro.js
+++ 3º - Semestre/Desenvolvimento WEB II/Desenvolvimento React/primeiro-site/src/Components/Cadastro.js
@@ -1,45 +1,99 @@
 import { useState } from "react";
 
 function Cadastro() {
-  const [nome, setNome] = useState("Nome");
-  const [email, setEmail] = useState("Email");
-  const [idade, setIdade] = useState("Idade");
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [idade, setIdade] = useState("");
+  const [user, setUser] = useState({});
+  const [tarefas, setTarefas] = useState([
+    "Pagar a conta de luz",
+    "Estudar Programação",
+    "Enviar Tarefa",
+  ]);
 
-  function handleChangeName(nome) {
-    setNome(nome);
+  const [input, setInput] = useState("");
+
+  function handleRegistro(e) {
+    e.preventDefault();
+    setUser({
+      nome: nome,
+      idade: idade,
+      email: email,
+    });
   }
 
-  function handleChangeEmail(email) {
-    setEmail(email);
-  }
+  function handleTarefa(e) {
+    e.preventDefault();
 
-  function handleChangeIdade(idade) {
-    setIdade(idade);
+    setTarefas([...tarefas, input]);
+    setInput("");
   }
 
   return (
     <div>
       <div>
-        <form>
-          <label>Digite seu Nome: </label>
-          <input></input>
+        <h2>Cadastro de Tarefas</h2>
+        <form onSubmit={handleTarefa}>
+          <label>Nome da tarefa: </label>
+          <br />
+          <input
+            placeholder="Digite uma tarefa"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <br />
+          <button type="submit">Cadastra Tarefa</button>
+          <br />
         </form>
+        <ul>
+          {tarefas.map((tarefa) => (
+            <li>{tarefa}</li>
+          ))}
+        </ul>
       </div>
+
+      <form onSubmit={handleRegistro}>
+        <hr />
+
+        <h2>Cadastro</h2>
+
+        <label>Nome: </label>
+        <br />
+        <input
+          placeholder="Digite seu Nome"
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        />
+        <br />
+        <label>Email: </label>
+        <br />
+        <input
+          placeholder="Digite seu Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <label>Idade: </label>
+        <br />
+        <input
+          placeholder="Digite sua Idade"
+          value={idade}
+          onChange={(e) => setIdade(e.target.value)}
+        />
+        <br />
+        <button type="submit">Registro</button>
+      </form>
+      <br />
+      <br />
       <div>
-        <form>
-          <label>Digite seu E-Mail: </label>
-          <input></input>
-        </form>
+        <span>Bem vindo, {user.nome}</span>
+        <br />
+        <span>Idade: {user.idade}</span>
+        <br />
+        <span>Email: {user.email}</span>
+        <br />
       </div>
-      <div>
-        <form>
-          <label>Digite sua Idade: </label>
-          <input></input>
-        </form>
-      </div>
-      <button type="submit">Cadastrar</button>
     </div>
   );
 }
-
 export default Cadastro;
